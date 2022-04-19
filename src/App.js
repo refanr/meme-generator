3922791c7e09a39eea8c61cf5f158cb0d55e45e8
refanr <reynirg20@ru.is>
2022-04-19 23:04:42 +0000

    fikti

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Meme from "./components/Meme"
+
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <Meme />
         </div>
     )
 }
